e5e3c3ef648b684189a8a0f899a5810df1ec6a04
phamhongduc <nroduc.contact@email.com>
2025-04-08 01:44:05 +0700

    feat: add custom label to production status chart for displaying total production orders

--- src/app/dashboard/_components/charts/production-status-chart.tsx
+++ src/app/dashboard/_components/charts/production-status-chart.tsx
@@ -63,7 +63,24 @@ export default function ProductionStatusChart() {
               stroke='#fff'
               cornerRadius={10}
               className='aspect-[1/1]'
-            ></Pie>
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
+                    return (
+                      <text x={viewBox.cx} y={viewBox.cy} textAnchor='middle' dominantBaseline='middle'>
+                        <tspan x={viewBox.cx} y={viewBox.cy} className='fill-foreground text-3xl font-bold'>
+                          {total.toLocaleString()}
+                        </tspan>
+                        <tspan x={viewBox.cx} y={(viewBox.cy || 0) + 24} className='fill-muted-foreground'>
+                          Lệnh sản xuất
+                        </tspan>
+                      </text>
+                    )
+                  }
+                }}
+              />
+            </Pie>
           </PieChart>
         </ChartContainer>
 
