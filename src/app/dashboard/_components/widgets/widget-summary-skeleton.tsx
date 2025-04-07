dd42cd954aa2132a1ce67c5485735e98b45835ab
phamhongduc <nroduc.contact@email.com>
2025-04-08 00:24:48 +0700

    feat: enhance WidgetSummarySkeleton with updated skeleton styles and improved layout

--- src/app/dashboard/_components/widgets/widget-summary-skeleton.tsx
+++ src/app/dashboard/_components/widgets/widget-summary-skeleton.tsx
@@ -7,12 +7,12 @@ export default function WidgetSummarySkeleton() {
   return (
     <CardContent>
       <div className='flex shrink-0 grow basis-0 items-start justify-between'>
-        <div className='flex flex-col items-start justify-center'>
-          <Skeleton className='h-[32px] w-[128px]' />
-          <Skeleton className='h-[20px] w-[128px]' />
+        <div className='flex flex-col items-start justify-center gap-2'>
+          <Skeleton className='h-[40px] w-[24px] font-["Barlow"] text-[32px] leading-12 font-bold text-[#0375F3]' />
+          <Skeleton className='h-[20px] w-[128px] font-["Lexend_Deca"] text-sm leading-[20px] font-normal text-[#141522]' />
         </div>
         <div className='flex items-center gap-1'>
-          <Skeleton className='h-[24px] w-[24px]' />
+          <Skeleton className='h-[20px] w-[24px]' />
           <Skeleton className='h-[20px] w-[20px]' />
         </div>
       </div>
